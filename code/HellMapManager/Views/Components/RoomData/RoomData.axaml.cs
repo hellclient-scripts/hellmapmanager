1f7187ece28d2f9add8cfc77fe72bd6a5ef65a53
Jarlyyn <jarlyyn@qq.com>
2025-04-17 15:22:45 +0800

    update

---
+++ code/HellMapManager/Views/Components/RoomData/RoomData.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HellMapManager.Views.Components.RoomData;
+
+public partial class RoomData : UserControl
+{
+    public RoomData()
+    {
+        InitializeComponent();
+    }
+}
