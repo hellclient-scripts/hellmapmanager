5fc1739048ea630b9d23d07a8aa8ba0f36e38a5b
Jarlyyn <jarlyyn@qq.com>
2025-05-12 18:18:23 +0800

    update

--- code/HellMapManager/Models/Environment.cs
+++ code/HellMapManager/Models/Environment.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using HarfBuzzSharp;
 
 
 namespace HellMapManager.Models;
@@ -19,25 +22,152 @@ public class Environment
     public List<Condition> RoomConditions = [];
     public List<Room> Rooms = [];
     public List<Path> Paths = [];
-    public bool DisableShortcuts = true;
-    public List<Exit> Shortcuts = [];
+    public List<RoomConditionExit> Shortcuts = [];
     public List<string> Whitelist = [];
-    public List<string> Blocklist = [];
+    public List<string> Blacklist = [];
     public List<Link> BlockedLinks = [];
+    public bool DisableShortcuts = false;
+
     public int MaxExitCost = 0;
 }
 
 public class Context
 {
-    public Dictionary<string, bool> Tags = new();
-    public Dictionary<string, bool> RoomConditions = new();
-    public Dictionary<string, Room> Rooms = new();
-    public bool DisableShortcuts = true;
-    public Dictionary<string, bool> Whitelist = new();
-    public Dictionary<string, bool> Blocklist = new();
-    public List<Exit> Shortcuts = [];
+    public Dictionary<string, bool> Tags = [];
+    public Dictionary<string, bool> RoomConditions = [];
+    public Dictionary<string, Room> Rooms = [];
+    public Dictionary<string, bool> Whitelist = [];
+    public Dictionary<string, bool> Blacklist = [];
+    public List<RoomConditionExit> Shortcuts = [];
+    public Dictionary<string, List<Path>> Paths = [];
+    public Dictionary<string, Dictionary<string, bool>> BlockedLinks = [];
+    public bool DisableShortcuts = false;
 
     public int MaxExitCost = 0;
+    public Context ClearTags()
+    {
+        Tags.Clear();
+        return this;
+    }
+    public Context WithTags(List<string> tags)
+    {
+        foreach (var tag in tags)
+        {
+            Tags[tag] = true;
+        }
+        return this;
+    }
+    public Context ClearRoomConditions()
+    {
+        RoomConditions.Clear();
+        return this;
+    }
+    public Context WithRoomConditions(List<Condition> conditions)
+    {
+        foreach (var condition in conditions)
+        {
+            RoomConditions[condition.Key] = condition.Not;
+        }
+        return this;
+
+    }
+    public Context ClearRooms()
+    {
+        Rooms.Clear();
+        return this;
+    }
+    public Context WithRooms(List<Room> rooms)
+    {
+        foreach (var room in rooms)
+        {
+            Rooms[room.Key] = room;
+        }
+        return this;
+    }
+    public Context ClearWhiteList()
+    {
+        Whitelist.Clear();
+        return this;
+    }
+    public Context WithWhiteList(List<string> list)
+    {
+        foreach (var item in list)
+        {
+            Whitelist[item] = true;
+        }
+        return this;
+    }
+    public Context ClearBlackList()
+    {
+        Whitelist.Clear();
+        return this;
+    }
+    public Context WithBlackList(List<string> list)
+    {
+        foreach (var item in list)
+        {
+            Blacklist[item] = true;
+        }
+        return this;
+    }
+    public Context ClearShortcuts()
+    {
+        Shortcuts.Clear();
+        return this;
+    }
+    public Context WithShortcuts(List<RoomConditionExit> list)
+    {
+        Shortcuts = (List<RoomConditionExit>)Shortcuts.Concat(list);
+        return this;
+    }
+    public Context ClearPaths()
+    {
+        Paths.Clear();
+        return this;
+    }
+    public Context WithPaths(List<Path> list)
+    {
+        foreach (var item in list)
+        {
+
+            if (Paths.TryGetValue(item.From, out var paths))
+            {
+                paths = (List<Path>)paths.Concat(list);
+            }
+            else
+            {
+                Paths[item.From] = [.. list];
+            }
+
+        }
+        return this;
+    }
+    public Context ClearBlockedLinks()
+    {
+        BlockedLinks.Clear();
+        return this;
+    }
+    public Context WithBlockedLinks(List<Link> list)
+    {
+        foreach (var link in list)
+        {
+            if (!BlockedLinks.ContainsKey(link.From))
+            {
+                BlockedLinks[link.From] = new Dictionary<string, bool>();
+            }
+            BlockedLinks[link.From][link.To] = true;
+        }
+        return this;
+    }
+    public Context WithDisableShortcuts(bool val)
+    {
+        this.DisableShortcuts = val;
+        return this;
+    }
+    public bool IsBlocked(string from, string to)
+    {
+        return BlockedLinks.ContainsKey(from) && BlockedLinks[from].ContainsKey(to);
+    }
     public bool ValidateExit(Exit exit, MapFile mf)
     {
         //Todo
