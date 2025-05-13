6fa986fd67ebefda22ee2a5c036b7e89fa8bbfea
Jarlyyn <jarlyyn@qq.com>
2025-05-13 12:56:43 +0800

    update

--- code/HellMapManager/Adapters/SystemAdapter.cs
+++ code/HellMapManager/Adapters/SystemAdapter.cs
@@ -3,6 +3,10 @@ namespace HellMapManager.Adapters;
 public class SystemAdapter
 {
     public static readonly SystemAdapter Instance = new();
+    public void Reset()
+    {
+        File = new FileAdapter();
+    }
     public IFileAdapter File = new FileAdapter();
 }
 
