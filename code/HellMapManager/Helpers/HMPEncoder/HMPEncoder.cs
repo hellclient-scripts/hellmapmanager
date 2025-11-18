665c646449c050f54641365590cfd2cbd74a041b
Jarlyyn <jarlyyn@qq.com>
2025-11-18 16:59:01 +0800

    fix

---
+++ code/HellMapManager/Helpers/HMPEncoder/HMPEncoder.cs
@@ -0,0 +1,16 @@
+using HellMapManager.Models;
+
+namespace HellMapManager.Helpers.HMPEncoder;
+
+public class HMPEncoder
+{
+    public static byte[] Encode(Diffs diffs)
+    {
+        return [];
+    }
+    public static Diffs Decode(byte[] data)
+    {
+        var result = new Diffs();
+        return result;
+    }
+}
--- code/HellMapManager/Helpers/HMPFile.cs
+++ code/HellMapManager/Helpers/HMPFile.cs
@@ -1,1 +1,36 @@
+using System.IO;
+using System.IO.Compression;
+
+using HellMapManager.Models;
+using HellMapManager.Cores;
 namespace HellMapManager.Helpers;
+
+using HellMapManager.Adapters;
+
+public class HMPFile
+{
+    public static void Save(string name, Diffs df)
+    {
+        var result = HMPEncoder.HMPEncoder.Encode(df);
+        using var zipToOpen = SystemAdapter.File.WriteStream(name);
+        using ZipArchive archive = new(zipToOpen, ZipArchiveMode.Update);
+        ZipArchiveEntry hmmEntry = archive.CreateEntry(AppPreset.ZipEnityName);
+        using var memoryStream = new MemoryStream(result);
+        memoryStream.CopyTo(hmmEntry.Open());
+    }
+    public static Diffs? Open(string name)
+    {
+        byte[] body;
+        using var zipToOpen = SystemAdapter.File.ReadStream(name);
+        using ZipArchive archive = new(zipToOpen, ZipArchiveMode.Read);
+        ZipArchiveEntry? hmmEntry = archive.GetEntry(AppPreset.ZipEnityName);
+        if (hmmEntry is null)
+        {
+            return null;
+        }
+        using var memoryStream = new MemoryStream();
+        hmmEntry.Open().CopyTo(memoryStream);
+        body = memoryStream.ToArray();
+        return HMPEncoder.HMPEncoder.Decode(body);
+    }
+}
