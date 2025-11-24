f0f49a8375e67a1c31225a5ab3c7d5a1d31d0c9d
Jarlyyn <jarlyyn@qq.com>
2025-11-25 01:10:03 +0800

    update

---
+++ code/HellMapManager/Windows/PatchWindow/SnapshotView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HellMapManager.Windows.PatchWindow;
+
+public partial class SnapshotView : UserControl
+{
+    public SnapshotView()
+    {
+        InitializeComponent();
+    }
+}
