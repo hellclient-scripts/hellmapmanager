f8a3538564b31ce49424e949d290c5c9a3656ac6
Jarlyyn <jarlyyn@qq.com>
2025-11-25 00:17:20 +0800

    update

---
+++ code/HellMapManager/Windows/PatchWindow/LandmarkView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HellMapManager.Windows.PatchWindow;
+
+public partial class LandmarkView : UserControl
+{
+    public LandmarkView()
+    {
+        InitializeComponent();
+    }
+}
