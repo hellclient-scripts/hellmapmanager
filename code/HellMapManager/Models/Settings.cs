8894276088ad1d03071f5cd3e24ceee6c7bec1dc
Jarlyyn <jarlyyn@qq.com>
2025-12-06 19:24:17 +0800

    APIConfig class

--- code/HellMapManager/Models/Settings.cs
+++ code/HellMapManager/Models/Settings.cs
@@ -18,3 +18,29 @@ public class Settings
     public bool APIEnabled { get; set; } = false;
     public string BuildURL() => $"http://localhost:{GetPort()}/";
 }
+
+public class APIConfig
+{
+    public static APIConfig From(Settings settings)
+    {
+        return new APIConfig()
+        {
+            APIPort = settings.APIPort,
+            APIUserName = settings.APIUserName,
+            APIPassWord = settings.APIPassWord,
+            APIEnabled = settings.APIEnabled,
+        };
+    }
+    public void Apply(Settings settings)
+    {
+        settings.APIPort = APIPort;
+        settings.APIUserName = APIUserName;
+        settings.APIPassWord = APIPassWord;
+        settings.APIEnabled = APIEnabled;
+    }
+    public int APIPort { get; set; } = 0;
+    public string APIUserName { get; set; } = "";
+    public string APIPassWord { get; set; } = "";
+    public bool APIEnabled { get; set; } = false;
+
+}
